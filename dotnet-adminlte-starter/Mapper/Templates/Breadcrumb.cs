afe8d1b10b2df92827f18f79a85da8585b4d2a81
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2023-01-17 15:19:00 +0700

    update template
    update template

---
+++ dotnet-adminlte-starter/Mapper/Templates/Breadcrumb.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_adminlte_starter.Mapper.Templates
+{
+    public class Breadcrumb
+    {
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+    }
+}
---
+++ dotnet-adminlte-starter/Mapper/Templates/TitleHeader.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet_adminlte_starter.Mapper.Templates
+{
+    public class TitleHeader
+    {
+        public string? Name { get; set; }
+        public Breadcrumb[]? Breadcrumbs { get; set; }
+    }
+}
